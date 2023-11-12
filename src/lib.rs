d98524d0d714e047d4468f7de0cc569cc7d42c8d
Inseok Lee <git@dlun.ch>
2023-11-12 14:39:03 +0900

    parse java class

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,21 @@
+#![no_std]
+extern crate alloc;
 
+use cafebabe::parse_class;
+
+pub type JvmResult<T> = anyhow::Result<T>;
+
+#[derive(Default)]
+pub struct Jvm {}
+
+impl Jvm {
+    pub fn new() -> Jvm {
+        Jvm {}
+    }
+
+    pub fn load_class(&mut self, class_data: &[u8]) -> JvmResult<()> {
+        let class = parse_class(class_data)?;
+
+        todo!("Loaded class {:?}", class);
+    }
+}
