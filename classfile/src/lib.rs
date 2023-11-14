e9272090303827753459d43163d0f9bd30ad8709
Inseok Lee <git@dlun.ch>
2023-11-14 19:50:11 +0900

    Add basic classfile parser

--- classfile/src/lib.rs
+++ classfile/src/lib.rs
@@ -1,7 +1,127 @@
+#![no_std]
+extern crate alloc;
+
+use alloc::vec::Vec;
+
+use nom::{combinator::map, number::complete::be_u16};
+use nom_derive::{NomBE, Parse};
+
 type ClassFileResult<T> = anyhow::Result<T>;
 
-pub struct ClassFile {}
+#[derive(NomBE)]
+#[nom(Exact)]
+#[nom(Complete)]
+pub struct ClassFile {
+    #[nom(Verify = "*magic == 0xCAFEBABE")]
+    pub magic: u32,
+    pub minor_version: u16,
+    pub major_version: u16,
+    #[nom(LengthCount = "map(be_u16, |x| x - 1)")]
+    pub constant_pool: Vec<ConstantPoolInfo>,
+    pub access_flags: u16,
+    pub this_class: u16,
+    pub super_class: u16,
+    #[nom(LengthCount = "be_u16")]
+    pub interfaces: Vec<u16>,
+    #[nom(LengthCount = "be_u16")]
+    pub fields: Vec<FieldInfo>,
+    #[nom(LengthCount = "be_u16")]
+    pub methods: Vec<MethodInfo>,
+    #[nom(LengthCount = "be_u16")]
+    pub attributes: Vec<AttributeInfo>,
+}
 
-pub fn parse_class(_file: &[u8]) -> ClassFileResult<ClassFile> {
-    Ok(ClassFile {})
+#[derive(NomBE)]
+pub struct ConstantUtf8Info {
+    pub length: u16,
+    #[nom(Count = "length")]
+    pub bytes: Vec<u8>,
+}
+
+#[derive(NomBE)]
+pub struct ConstantClassInfo {
+    pub name_index: u16,
+}
+
+#[derive(NomBE)]
+pub struct ConstantStringInfo {
+    pub string_index: u16,
+}
+
+#[derive(NomBE)]
+pub struct ConstantReferenceInfo {
+    pub class_index: u16,
+    pub name_and_type_index: u16,
+}
+
+#[derive(NomBE)]
+pub struct ConstantNameAndTypeInfo {
+    pub name_index: u16,
+    pub descriptor_index: u16,
+}
+
+#[derive(NomBE)]
+#[nom(Selector = "u8")]
+pub enum ConstantPoolValue {
+    #[nom(Selector = "1")]
+    Utf8(ConstantUtf8Info),
+    #[nom(Selector = "3")]
+    Integer(u32),
+    #[nom(Selector = "4")]
+    Float(f32),
+    #[nom(Selector = "5")]
+    Long(u64),
+    #[nom(Selector = "6")]
+    Double(f64),
+    #[nom(Selector = "7")]
+    Class(ConstantClassInfo),
+    #[nom(Selector = "8")]
+    String(ConstantStringInfo),
+    #[nom(Selector = "9")]
+    Fieldref(ConstantReferenceInfo),
+    #[nom(Selector = "10")]
+    Methodref(ConstantReferenceInfo),
+    #[nom(Selector = "11")]
+    InstanceMethodref(ConstantReferenceInfo),
+    #[nom(Selector = "12")]
+    NameAndType(ConstantNameAndTypeInfo),
+}
+
+#[derive(NomBE)]
+pub struct ConstantPoolInfo {
+    pub tag: u8,
+    #[nom(Parse = "{ |x| ConstantPoolValue::parse(x, tag) }")]
+    pub value: ConstantPoolValue,
+}
+
+#[derive(NomBE)]
+pub struct FieldInfo {
+    pub access_flags: u16,
+    pub name_index: u16,
+    pub descriptor_index: u16,
+    #[nom(LengthCount = "be_u16")]
+    pub attributes: Vec<AttributeInfo>,
+}
+
+#[derive(NomBE)]
+pub struct MethodInfo {
+    pub access_flags: u16,
+    pub name_index: u16,
+    pub descriptor_index: u16,
+    #[nom(LengthCount = "be_u16")]
+    pub attributes: Vec<AttributeInfo>,
+}
+
+#[derive(NomBE)]
+pub struct AttributeInfo {
+    pub attribute_name_index: u16,
+    pub attribute_length: u32,
+    #[nom(Count = "attribute_length")]
+    pub info: Vec<u8>,
+}
+
+pub fn parse_class(file: &[u8]) -> ClassFileResult<ClassFile> {
+    let result = ClassFile::parse(file).map(|x| x.1).map_err(|e| anyhow::anyhow!("{}", e))?;
+
+    Ok(result)
 }
--- classfile/tests/test.rs
+++ classfile/tests/test.rs
@@ -4,7 +4,26 @@ use classfile::parse_class;
 fn test_hello() -> anyhow::Result<()> {
     let hello = include_bytes!("../../test_data/Hello.class");
 
-    parse_class(hello)?;
+    let class = parse_class(hello)?;
+
+    assert_eq!(class.magic, 0xCAFEBABE);
+    assert_eq!(class.major_version, 65);
+    assert_eq!(class.minor_version, 0);
+    assert_eq!(class.constant_pool.len(), 28);
+    assert_eq!(class.access_flags, 0x20);
+    assert_eq!(class.this_class, 0x15);
+    assert_eq!(class.super_class, 0x02);
+    assert_eq!(class.interfaces.len(), 0);
+    assert_eq!(class.fields.len(), 0);
+    assert_eq!(class.methods.len(), 2);
+    assert_eq!(class.attributes.len(), 1);
+
+    assert_eq!(class.methods[0].name_index, 5);
+    assert_eq!(class.methods[0].descriptor_index, 6);
+
+    assert_eq!(class.attributes[0].attribute_name_index, 27);
+    assert_eq!(class.attributes[0].attribute_length, 2);
+    assert_eq!(class.attributes[0].info, [0, 28]);
 
     Ok(())
 }
