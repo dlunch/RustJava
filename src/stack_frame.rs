9164201ca3f585bbce7a96e9871836f5a3eba48d
Inseok Lee <git@dlun.ch>
2023-11-15 21:29:13 +0900

    Add stack frame variables

--- src/stack_frame.rs
+++ src/stack_frame.rs
@@ -1,7 +1,17 @@
-pub struct StackFrame {}
+use alloc::vec::Vec;
+
+pub enum StackVariable {}
+
+pub struct StackFrame {
+    pub local_variables: Vec<StackVariable>,
+    pub operand_stack: Vec<StackVariable>,
+}
 
 impl StackFrame {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            local_variables: Vec::new(),
+            operand_stack: Vec::new(),
+        }
     }
 }
