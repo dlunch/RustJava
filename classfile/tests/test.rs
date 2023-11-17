bb5220ac945724e3eb8806f2619471c1a2916980
Inseok Lee <git@dlun.ch>
2023-11-17 20:31:06 +0900

    Test opcode

--- classfile/tests/test.rs
+++ classfile/tests/test.rs
@@ -20,6 +20,28 @@ fn test_hello() -> anyhow::Result<()> {
 
     assert_eq!(class.methods[0].name, "<init>");
     assert_eq!(class.methods[0].descriptor, "()V");
+    assert!(matches!(class.methods[0].attributes[0], AttributeInfo::Code { .. }));
+    if let AttributeInfo::Code(x) = &class.methods[0].attributes[0] {
+        assert_eq!(x.code.len(), 3);
+        assert!(matches!(x.code.get(&0).unwrap(), classfile::Opcode::Aload0));
+        assert!(matches!(x.code.get(&1).unwrap(), classfile::Opcode::Invokespecial(1)));
+        assert!(matches!(x.code.get(&4).unwrap(), classfile::Opcode::Return));
+    } else {
+        panic!("Expected code attribute");
+    }
+
+    assert_eq!(class.methods[1].name, "main");
+    assert_eq!(class.methods[1].descriptor, "([Ljava/lang/String;)V");
+    assert!(matches!(class.methods[1].attributes[0], AttributeInfo::Code { .. }));
+    if let AttributeInfo::Code(x) = &class.methods[1].attributes[0] {
+        assert_eq!(x.code.len(), 4);
+        assert!(matches!(x.code.get(&0).unwrap(), classfile::Opcode::Getstatic(7)));
+        assert!(matches!(x.code.get(&3).unwrap(), classfile::Opcode::Ldc(13)));
+        assert!(matches!(x.code.get(&5).unwrap(), classfile::Opcode::Invokevirtual(15)));
+        assert!(matches!(x.code.get(&8).unwrap(), classfile::Opcode::Return));
+    } else {
+        panic!("Expected code attribute");
+    }
 
     assert!(matches!(class.attributes[0], AttributeInfo::SourceFile { .. }));
 
