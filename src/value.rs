7d3073815a800b17482779b9b56b5386a279bd18
Inseok Lee <git@dlun.ch>
2023-11-18 10:28:00 +0900

    Add more JavaValue variants

--- src/value.rs
+++ src/value.rs
@@ -1,3 +1,13 @@
+use alloc::rc::Rc;
+use core::cell::RefCell;
+
+use crate::class::ClassInstance;
+
 pub enum JavaValue {
     Void,
+    Integer(i32),
+    Long(i64),
+    Float(f32),
+    Double(f64),
+    ClassInstance(Rc<RefCell<ClassInstance>>),
 }
