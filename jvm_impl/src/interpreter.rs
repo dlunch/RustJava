3fc67e060efb231ad83ffae078d2c3e5b1fdd7c5
Inseok Lee <git@dlun.ch>
2023-12-29 11:33:20 +0900

    Sort match by alphabetical order

--- jvm_impl/src/interpreter.rs
+++ jvm_impl/src/interpreter.rs
@@ -19,10 +19,17 @@ impl Interpreter {
         let mut iter = bytecode.range(0..);
         while let Some((_, opcode)) = iter.next() {
             match opcode {
-                Opcode::Ldc(x) => stack_frame.operand_stack.push(Self::constant_to_value(jvm, x).await?),
+                Opcode::Dup => {
+                    let value = stack_frame.operand_stack.pop().unwrap();
+                    stack_frame.operand_stack.push(value.clone());
+                    stack_frame.operand_stack.push(value);
+                }
                 Opcode::Getstatic(x) => stack_frame
                     .operand_stack
                     .push(jvm.get_static_field(&x.class, &x.name, &x.descriptor).await?),
+                Opcode::Goto(x) => {
+                    iter = bytecode.range(*x as u32..);
+                }
                 Opcode::Invokespecial(x) | Opcode::Invokevirtual(x) => {
                     // TODO there's difference between invokespecial/invokevirtual, but we ignore it for now..
                     let method_type = JavaType::parse(&x.descriptor);
@@ -37,18 +44,11 @@ impl Interpreter {
 
                     jvm.invoke_method(&instance, &x.class, &x.name, &x.descriptor, &params).await?;
                 }
+                Opcode::Ldc(x) => stack_frame.operand_stack.push(Self::constant_to_value(jvm, x).await?),
                 Opcode::New(x) => {
                     let class = jvm.instantiate_class(x.as_class()).await?;
 
                     stack_frame.operand_stack.push(JavaValue::Object(Some(class)));
-                }
-                Opcode::Dup => {
-                    let value = stack_frame.operand_stack.pop().unwrap();
-                    stack_frame.operand_stack.push(value.clone());
-                    stack_frame.operand_stack.push(value);
-                }
-                Opcode::Goto(x) => {
-                    iter = bytecode.range(*x as u32..);
                 }
                 Opcode::Return => {
                     return Ok(JavaValue::Void);
