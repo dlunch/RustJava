4a159655856e6f8c8f6cc71ae7b9b7e40fb95a77
Inseok Lee <git@dlun.ch>
2024-01-14 15:32:33 +0900

    Implement class loading on classpathclassloader

--- java_runtime/src/classes/rustjava/class_path_class_loader.rs
+++ java_runtime/src/classes/rustjava/class_path_class_loader.rs
@@ -1,7 +1,9 @@
 use alloc::vec;
 
-use java_class_proto::{JavaMethodProto, JavaResult};
-use jvm::{ClassInstanceRef, Jvm};
+use bytemuck::cast_slice;
+
+use java_class_proto::{JavaFieldProto, JavaMethodProto, JavaResult};
+use jvm::{Array, ClassInstanceRef, Jvm};
 
 use crate::{
     classes::java::lang::{Class, ClassLoader, String},
@@ -19,8 +21,14 @@ impl ClassPathClassLoader {
             methods: vec![
                 JavaMethodProto::new("<init>", "(Ljava/lang/ClassLoader;)V", Self::init, Default::default()),
                 JavaMethodProto::new("findClass", "(Ljava/lang/String;)Ljava/lang/Class;", Self::find_class, Default::default()),
+                JavaMethodProto::new("addClassFile", "(Ljava/lang/String;[B)V", Self::add_class_file, Default::default()),
+                JavaMethodProto::new("addJarFile", "([B)V", Self::add_jar_file, Default::default()),
             ],
-            fields: vec![],
+            fields: vec![
+                JavaFieldProto::new("class_file_name", "Ljava/lang/String;", Default::default()),
+                JavaFieldProto::new("class_file", "[B", Default::default()),
+                JavaFieldProto::new("jar_file", "[B", Default::default()),
+            ],
         }
     }
 
@@ -34,15 +42,67 @@ impl ClassPathClassLoader {
     }
 
     async fn find_class(
-        _jvm: &mut Jvm,
-        _runtime: &mut RuntimeContext,
+        jvm: &mut Jvm,
+        runtime: &mut RuntimeContext,
         this: ClassInstanceRef<Self>,
         name: ClassInstanceRef<String>,
     ) -> JavaResult<ClassInstanceRef<Class>> {
         tracing::debug!("rustjava.ClassPathClassLoader::findClass({:?}, {:?})", &this, name);
 
-        // TODO
+        let name = String::to_rust_string(jvm, &name)?;
+
+        let class_file_name: ClassInstanceRef<String> = jvm.get_field(&this, "class_file_name", "Ljava/lang/String;")?;
+        if !class_file_name.is_null() {
+            let class_file_name = String::to_rust_string(jvm, &class_file_name)?;
+
+            if name == class_file_name {
+                let class_file = jvm.get_field(&this, "class_file", "[B")?;
+                let length = jvm.array_length(&class_file)?;
+
+                let class_file_data = jvm.load_byte_array(&class_file, 0, length)?;
+
+                let _class = runtime.define_class(&name, cast_slice(&class_file_data));
+                // TODO proper java/lang/class
+
+                let java_class = jvm.new_class("java/lang/Class", "()V", ()).await?;
+                return Ok(java_class.into());
+            }
+        }
+
+        let jar_file: ClassInstanceRef<Array<i8>> = jvm.get_field(&this, "jar_file", "[B")?;
+        if !jar_file.is_null() {
+            // TODO
+        }
 
         Ok(None.into())
+    }
+
+    // we don't have classpath (yet), so we need backdoor to add classes to loader
+    async fn add_class_file(
+        jvm: &mut Jvm,
+        _runtime: &mut RuntimeContext,
+        mut this: ClassInstanceRef<Self>,
+        file_name: ClassInstanceRef<String>,
+        data: ClassInstanceRef<Array<i8>>,
+    ) -> JavaResult<()> {
+        tracing::debug!("rustjava.ClassPathClassLoader::addClassFile({:?})", &this);
+
+        jvm.put_field(&mut this, "class_file_name", "Ljava/lang/String;", file_name)?;
+        jvm.put_field(&mut this, "class_file", "[B", data)?; // TODO multiple classes
+
+        Ok(())
+    }
+
+    async fn add_jar_file(
+        jvm: &mut Jvm,
+        _runtime: &mut RuntimeContext,
+        mut this: ClassInstanceRef<Self>,
+        data: ClassInstanceRef<Array<i8>>,
+    ) -> JavaResult<()> {
+        tracing::debug!("rustjava.ClassPathClassLoader::addJarFile({:?})", &this);
+
+        jvm.put_field(&mut this, "jar_file", "[B", data)?; // TODO multiple jars
+
+        Ok(())
     }
 }
