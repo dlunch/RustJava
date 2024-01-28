4b3da958889b843b0fe92434546d308960a772b3
Inseok Lee <git@dlun.ch>
2024-01-28 15:58:10 +0900

    Reorganize test data

--- tests/jar.rs
+++ tests/jar.rs
@@ -4,7 +4,7 @@ use test_helper::run_jar;
 
 #[futures_test::test]
 async fn test_jar() -> anyhow::Result<()> {
-    let jar = include_bytes!("../test_data/jar/test.jar");
+    let jar = include_bytes!("../test_data/test.jar");
 
     let result = run_jar(jar, &[]).await?;
     assert_eq!(result, "test content\n\n");
