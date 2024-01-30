c626e3401dbd6d4675958154a4fb0ca58cd1c3f2
Inseok Lee <git@dlun.ch>
2024-01-30 21:39:56 +0900

    Add java/lang/Object stubs

--- java_runtime/src/classes/java/lang/object.rs
+++ java_runtime/src/classes/java/lang/object.rs
@@ -1,9 +1,10 @@
 use alloc::{boxed::Box, vec};
 
 use java_class_proto::{JavaMethodProto, JavaResult};
+use java_constants::MethodAccessFlags;
 use jvm::{ClassInstance, ClassInstanceRef, Jvm};
 
-use crate::{RuntimeClassProto, RuntimeContext};
+use crate::{classes::java::lang::String, RuntimeClassProto, RuntimeContext};
 
 // class java.lang.Object
 pub struct Object {}
@@ -15,7 +16,17 @@ impl Object {
             interfaces: vec![],
             methods: vec![
                 JavaMethodProto::new("<init>", "()V", Self::init, Default::default()),
-                JavaMethodProto::new("getClass", "()Ljava/lang/Class;", Self::get_class, Default::default()),
+                JavaMethodProto::new("getClass", "()Ljava/lang/Class;", Self::get_class, MethodAccessFlags::NATIVE),
+                JavaMethodProto::new("hashCode", "()I", Self::hash_code, MethodAccessFlags::NATIVE),
+                JavaMethodProto::new("equals", "(Ljava/lang/Object;)Z", Self::equals, Default::default()),
+                JavaMethodProto::new("clone", "()Ljava/lang/Object;", Self::java_clone, MethodAccessFlags::NATIVE),
+                JavaMethodProto::new("toString", "()Ljava/lang/String;", Self::to_string, Default::default()),
+                JavaMethodProto::new("notify", "()V", Self::notify, MethodAccessFlags::NATIVE),
+                JavaMethodProto::new("notifyAll", "()V", Self::notify_all, MethodAccessFlags::NATIVE),
+                JavaMethodProto::new("wait", "(J)V", Self::wait_long, MethodAccessFlags::NATIVE),
+                JavaMethodProto::new("wait", "(JI)V", Self::wait_long_int, Default::default()),
+                JavaMethodProto::new("wait", "()V", Self::wait, Default::default()),
+                JavaMethodProto::new("finalize", "()V", Self::finalize, Default::default()),
             ],
             fields: vec![],
         }
@@ -28,7 +39,7 @@ impl Object {
     }
 
     async fn get_class(jvm: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>) -> JavaResult<ClassInstanceRef<Self>> {
-        tracing::warn!("java.lang.Object::getClass({:?})", &this);
+        tracing::debug!("java.lang.Object::getClass({:?})", &this);
 
         // TODO can we get class directly?
         let this: Box<dyn ClassInstance> = this.into();
@@ -37,5 +48,67 @@ impl Object {
         let class = jvm.resolve_class(&class_name).await?.unwrap().java_class(jvm).await?;
 
         Ok(class.into())
+    }
+
+    async fn hash_code(_: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>) -> JavaResult<i32> {
+        tracing::warn!("stub java.lang.Object::hashCode({:?})", &this);
+
+        todo!()
+    }
+
+    async fn equals(_: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>, other: ClassInstanceRef<Self>) -> JavaResult<bool> {
+        tracing::warn!("stub java.lang.Object::equals({:?}, {:?})", &this, &other);
+
+        todo!()
+    }
+
+    async fn java_clone(_: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>) -> JavaResult<ClassInstanceRef<Self>> {
+        tracing::warn!("stub java.lang.Object::clone({:?})", &this);
+
+        todo!()
+    }
+
+    async fn to_string(_: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>) -> JavaResult<ClassInstanceRef<String>> {
+        tracing::warn!("stub java.lang.Object::toString({:?})", &this);
+
+        // TODO return getClass().getName() + "@" + Integer.toHexString(hashCode());
+
+        todo!()
+    }
+
+    async fn notify(_: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>) -> JavaResult<()> {
+        tracing::warn!("stub java.lang.Object::notify({:?})", &this);
+
+        todo!()
+    }
+
+    async fn notify_all(_: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>) -> JavaResult<()> {
+        tracing::warn!("stub java.lang.Object::notifyAll({:?})", &this);
+
+        todo!()
+    }
+
+    async fn wait_long(_: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>, millis: i64) -> JavaResult<()> {
+        tracing::warn!("stub java.lang.Object::wait({:?}, {:?})", &this, millis);
+
+        todo!()
+    }
+
+    async fn wait_long_int(_: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>, millis: i64, nanos: i32) -> JavaResult<()> {
+        tracing::warn!("stub java.lang.Object::wait({:?}, {:?}, {:?})", &this, millis, nanos);
+
+        todo!()
+    }
+
+    async fn wait(_: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>) -> JavaResult<()> {
+        tracing::warn!("stub java.lang.Object::wait({:?})", &this);
+
+        todo!()
+    }
+
+    async fn finalize(_: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>) -> JavaResult<()> {
+        tracing::warn!("stub java.lang.Object::finalize({:?})", &this);
+
+        todo!()
     }
 }
