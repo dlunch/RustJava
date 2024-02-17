2af2ed990fccf1561d95db5523533cb2821457bc
Inseok Lee <git@dlun.ch>
2024-02-17 14:37:27 +0900

    Make JvmError implements std::error::Error

--- jvm/src/error.rs
+++ jvm/src/error.rs
@@ -1,4 +1,8 @@
-use alloc::{boxed::Box, string::String};
+use alloc::{
+    boxed::Box,
+    fmt::{self, Display, Formatter},
+    string::String,
+};
 
 use crate::ClassInstance;
 
@@ -6,4 +10,24 @@ use crate::ClassInstance;
 pub enum JvmError {
     JavaException(Box<dyn ClassInstance>),
     FatalError(String),
+}
+
+impl Display for JvmError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            JvmError::JavaException(e) => write!(f, "Java exception: {:?}", e),
+            JvmError::FatalError(e) => write!(f, "Fatal error: {}", e),
+        }
+    }
+}
+
+// XXX until https://github.com/rust-lang/rust/issues/103765 fixed
+extern crate std;
+impl std::error::Error for JvmError {
+    fn description(&self) -> &str {
+        match self {
+            JvmError::JavaException(_) => "Java exception",
+            JvmError::FatalError(_) => "Fatal error",
+        }
+    }
 }
