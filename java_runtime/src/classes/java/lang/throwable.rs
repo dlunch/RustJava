e5c553cc34a53ecb60799b89337ebfc3666ad9ae
Inseok Lee <git@dlun.ch>
2024-03-01 15:21:47 +0900

    Add constructor to java/lang/Throwable

--- java_runtime/src/classes/java/lang/throwable.rs
+++ java_runtime/src/classes/java/lang/throwable.rs
@@ -1,6 +1,9 @@
 use alloc::vec;
 
-use crate::RuntimeClassProto;
+use java_class_proto::JavaMethodProto;
+use jvm::{ClassInstanceRef, Jvm, Result};
+
+use crate::{RuntimeClassProto, RuntimeContext};
 
 // class java.lang.Throwable
 pub struct Throwable {}
@@ -10,8 +13,14 @@ impl Throwable {
         RuntimeClassProto {
             parent_class: Some("java/lang/Object"),
             interfaces: vec![],
-            methods: vec![],
+            methods: vec![JavaMethodProto::new("<init>", "()V", Self::init, Default::default())],
             fields: vec![],
         }
+    }
+
+    async fn init(_: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>) -> Result<()> {
+        tracing::debug!("java.lang.Throwable::<init>({:?})", &this);
+
+        Ok(())
     }
 }
