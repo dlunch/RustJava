fcecb291c63b64424fa60a0a1275223813655cd6
Inseok Lee <git@dlun.ch>
2024-03-10 21:00:08 +0900

    Throw java exception on recoverable error

--- jvm/src/jvm.rs
+++ jvm/src/jvm.rs
@@ -23,7 +23,7 @@ use crate::{
     invoke_arg::InvokeArg,
     method::Method,
     r#type::JavaType,
-    runtime::{JavaLangClass, JavaLangClassLoader},
+    runtime::{JavaLangClass, JavaLangClassLoader, JavaLangString},
     value::JavaValue,
     Result,
 };
@@ -126,9 +126,14 @@ impl Jvm {
 
         let class = self.resolve_class(class_name).await?;
 
-        let field = class.definition.field(name, descriptor, true).unwrap(); // TODO NoSuchFieldError
-
-        Ok(class.definition.get_static_field(&*field)?.into())
+        let field = class.definition.field(name, descriptor, true);
+        if let Some(field) = field {
+            Ok(class.definition.get_static_field(&*field)?.into())
+        } else {
+            Err(self
+                .exception("java/lang/NoSuchFieldError", &format!("{}.{}:{}", class_name, name, descriptor))
+                .await)
+        }
     }
 
     pub async fn put_static_field<T>(&self, class_name: &str, name: &str, descriptor: &str, value: T) -> Result<()>
@@ -139,9 +144,15 @@ impl Jvm {
 
         let mut class = self.resolve_class(class_name).await?;
 
-        let field = class.definition.field(name, descriptor, true).unwrap(); // TODO NoSuchFieldError
+        let field = class.definition.field(name, descriptor, true);
 
-        class.definition.put_static_field(&*field, value.into())
+        if let Some(field) = field {
+            class.definition.put_static_field(&*field, value.into())
+        } else {
+            Err(self
+                .exception("java/lang/NoSuchFieldError", &format!("{}.{}:{}", class_name, name, descriptor))
+                .await)
+        }
     }
 
     pub async fn get_field<T>(&self, instance: &Box<dyn ClassInstance>, name: &str, descriptor: &str) -> Result<T>
@@ -150,9 +161,18 @@ impl Jvm {
     {
         tracing::trace!("Get field {}.{}:{}", instance.class_definition().name(), name, descriptor);
 
-        let field = self.find_field(&*instance.class_definition(), name, descriptor)?.unwrap(); // TODO NoSuchFieldError
+        let field = self.find_field(&*instance.class_definition(), name, descriptor)?;
 
-        Ok(instance.get_field(&*field)?.into())
+        if let Some(field) = field {
+            Ok(instance.get_field(&*field)?.into())
+        } else {
+            Err(self
+                .exception(
+                    "java/lang/NoSuchFieldError",
+                    &format!("{}.{}:{}", instance.class_definition().name(), name, descriptor),
+                )
+                .await)
+        }
     }
 
     pub async fn put_field<T>(&self, instance: &mut Box<dyn ClassInstance>, name: &str, descriptor: &str, value: T) -> Result<()>
@@ -161,9 +181,18 @@ impl Jvm {
     {
         tracing::trace!("Put field {}.{}:{} = {:?}", instance.class_definition().name(), name, descriptor, value);
 
-        let field = self.find_field(&*instance.class_definition(), name, descriptor)?.unwrap(); // TODO NoSuchFieldError
+        let field = self.find_field(&*instance.class_definition(), name, descriptor)?;
 
-        instance.put_field(&*field, value.into())
+        if let Some(field) = field {
+            instance.put_field(&*field, value.into())
+        } else {
+            Err(self
+                .exception(
+                    "java/lang/NoSuchFieldError",
+                    &format!("{}.{}:{}", instance.class_definition().name(), name, descriptor),
+                )
+                .await)
+        }
     }
 
     pub async fn invoke_static<T, U>(&self, class_name: &str, name: &str, descriptor: &str, args: T) -> Result<U>
@@ -175,13 +204,24 @@ impl Jvm {
 
         let class = self.resolve_class(class_name).await?;
 
-        let method = class.definition.method(name, descriptor).unwrap(); // TODO NoSuchMethodError
+        let method = class.definition.method(name, descriptor);
 
-        if !method.access_flags().contains(MethodAccessFlags::STATIC) {
-            return Err(JavaError::FatalError("NoSuchMethodError".into())); // TODO use real exception class
+        if let Some(method) = method {
+            if !method.access_flags().contains(MethodAccessFlags::STATIC) {
+                return Err(self
+                    .exception(
+                        "java/lang/IncompatibleClassChangeError",
+                        &format!("{}.{}:{}", class_name, name, descriptor),
+                    )
+                    .await);
+            }
+
+            Ok(method.run(self, args.into_arg()).await?.into())
+        } else {
+            Err(self
+                .exception("java/lang/NoSuchMethodError", &format!("{}.{}:{}", class_name, name, descriptor))
+                .await)
         }
-
-        Ok(method.run(self, args.into_arg()).await?.into())
     }
 
     pub async fn invoke_virtual<T, U>(&self, instance: &Box<dyn ClassInstance>, name: &str, descriptor: &str, args: T) -> Result<U>
@@ -192,7 +232,7 @@ impl Jvm {
         tracing::trace!("Invoke virtual {}.{}:{}", instance.class_definition().name(), name, descriptor);
 
         let class = instance.class_definition();
-        let method = self.find_virtual_method(&*class, name, descriptor, false)?;
+        let method = self.find_virtual_method(&*class, name, descriptor, false).await?;
 
         let args = iter::once(JavaValue::Object(Some(clone_box(&**instance))))
             .chain(args.into_iter())
@@ -202,6 +242,7 @@ impl Jvm {
     }
 
     // non-virtual
+    #[async_recursion::async_recursion(?Send)]
     pub async fn invoke_special<T, U>(&self, instance: &Box<dyn ClassInstance>, class_name: &str, name: &str, descriptor: &str, args: T) -> Result<U>
     where
         T: InvokeArg,
@@ -210,19 +251,31 @@ impl Jvm {
         tracing::trace!("Invoke special {}.{}:{}", class_name, name, descriptor);
 
         let class = self.resolve_class(class_name).await?;
-        let method = class.definition.method(name, descriptor).unwrap(); // TODO NoSuchMethodError
+        let method = class.definition.method(name, descriptor);
 
-        let args = iter::once(JavaValue::Object(Some(clone_box(&**instance))))
-            .chain(args.into_iter())
-            .collect::<Vec<_>>();
+        if let Some(method) = method {
+            let args = iter::once(JavaValue::Object(Some(clone_box(&**instance))))
+                .chain(args.into_iter())
+                .collect::<Vec<_>>();
 
-        if method.access_flags().contains(MethodAccessFlags::STATIC) {
-            return Err(JavaError::FatalError("NoSuchMethodError".into())); // TODO use real exception class
+            if method.access_flags().contains(MethodAccessFlags::STATIC) {
+                return Err(self
+                    .exception(
+                        "java/lang/IncompatibleClassChangeError",
+                        &format!("{}.{}:{}", class_name, name, descriptor),
+                    )
+                    .await);
+            }
+
+            Ok(method.run(self, args.into_boxed_slice()).await?.into())
+        } else {
+            Err(self
+                .exception("java/lang/NoSuchMethodError", &format!("{}.{}:{}", class_name, name, descriptor))
+                .await)
         }
-
-        Ok(method.run(self, args.into_boxed_slice()).await?.into())
     }
 
+    #[async_recursion::async_recursion(?Send)]
     pub async fn store_array<T, U>(&self, array: &mut Box<dyn ClassInstance>, offset: usize, values: T) -> Result<()>
     where
         T: IntoIterator<Item = U>,
@@ -230,10 +283,16 @@ impl Jvm {
     {
         tracing::trace!("Store array {} at offset {}", array.class_definition().name(), offset);
 
-        let array = array.as_array_instance_mut().unwrap(); // TODO IllegalArgumentException
+        let array = array.as_array_instance_mut();
 
-        let values = values.into_iter().map(|x| x.into()).collect::<Vec<_>>();
-        array.store(offset, values.into_boxed_slice())
+        if let Some(array) = array {
+            let values = values.into_iter().map(|x| x.into()).collect::<Vec<_>>();
+            array.store(offset, values.into_boxed_slice())?;
+
+            Ok(())
+        } else {
+            Err(self.exception("java/lang/IllegalArgumentException", "Not an array").await)
+        }
     }
 
     pub async fn load_array<T>(&self, array: &Box<dyn ClassInstance>, offset: usize, count: usize) -> Result<Vec<T>>
@@ -242,48 +301,69 @@ impl Jvm {
     {
         tracing::trace!("Load array {} at offset {}", array.class_definition().name(), offset);
 
-        let array = array.as_array_instance().unwrap(); // TODO IllegalArgumentException
+        let array = array.as_array_instance();
 
-        let values = array.load(offset, count)?;
+        if let Some(array) = array {
+            let values = array.load(offset, count)?;
 
-        Ok(iter::IntoIterator::into_iter(values).map(|x| x.into()).collect::<Vec<_>>())
+            Ok(iter::IntoIterator::into_iter(values).map(|x| x.into()).collect::<Vec<_>>())
+        } else {
+            Err(self.exception("java/lang/IllegalArgumentException", "Not an array").await)
+        }
     }
 
     pub async fn store_byte_array(&self, array: &mut Box<dyn ClassInstance>, offset: usize, values: Vec<i8>) -> Result<()> {
         tracing::trace!("Store array {} at offset {}", array.class_definition().name(), offset);
 
-        let array = array.as_array_instance_mut().unwrap(); // TODO IllegalArgumentException
+        let array = array.as_array_instance_mut();
 
-        array.store_bytes(offset, values.into_boxed_slice())
+        if let Some(array) = array {
+            array.store_bytes(offset, values.into_boxed_slice())
+        } else {
+            Err(self.exception("java/lang/IllegalArgumentException", "Not an array").await)
+        }
     }
 
     pub async fn load_byte_array(&self, array: &Box<dyn ClassInstance>, offset: usize, count: usize) -> Result<Vec<i8>> {
         tracing::trace!("Load array {} at offset {}", array.class_definition().name(), offset);
 
-        let array = array.as_array_instance().unwrap(); // TODO IllegalArgumentException
+        let array = array.as_array_instance();
 
-        let values = array.load_bytes(offset, count)?;
+        if let Some(array) = array {
+            let values = array.load_bytes(offset, count)?;
 
-        Ok(values)
+            Ok(values)
+        } else {
+            Err(self.exception("java/lang/IllegalArgumentException", "Not an array").await)
+        }
     }
 
     pub async fn array_length(&self, array: &Box<dyn ClassInstance>) -> Result<usize> {
         tracing::trace!("Get array length {}", array.class_definition().name());
 
-        let array = array.as_array_instance().unwrap(); // TODO IllegalArgumentException
+        let array = array.as_array_instance();
 
-        Ok(array.length())
+        if let Some(array) = array {
+            Ok(array.length())
+        } else {
+            Err(self.exception("java/lang/IllegalArgumentException", "Not an array").await)
+        }
     }
 
     pub async fn array_element_type(&self, array: &Box<dyn ClassInstance>) -> Result<JavaType> {
         tracing::trace!("Get array element type {}", array.class_definition().name());
 
-        let array = array.as_array_instance().unwrap(); // TODO IllegalArgumentException
-        let class = ArrayClassInstance::class_definition(array);
+        let array = array.as_array_instance();
 
-        let type_name = &class.name()[1..]; // TODO can we store JavaType on class?
+        if let Some(array) = array {
+            let class = ArrayClassInstance::class_definition(array);
 
-        Ok(JavaType::parse(type_name))
+            let type_name = &class.name()[1..]; // TODO can we store JavaType on class?
+
+            Ok(JavaType::parse(type_name))
+        } else {
+            Err(self.exception("java/lang/IllegalArgumentException", "Not an array").await)
+        }
     }
 
     // temporary until we have working gc
@@ -323,7 +403,7 @@ impl Jvm {
             return Ok(class);
         }
 
-        Err(JavaError::FatalError("NoClassDefFoundError".into())) // TODO use real exception class
+        Err(self.exception("java/lang/NoClassDefFoundError", class_name).await)
     }
 
     pub async fn register_class(&self, class: Box<dyn ClassDefinition>, class_loader: Option<Box<dyn ClassInstance>>) -> Result<()> {
@@ -345,6 +425,13 @@ impl Jvm {
         let instance_class = instance.class_definition();
 
         self.is_instance_by_name(&instance_class.name(), class_name).await
+    }
+
+    async fn exception(&self, r#type: &str, message: &str) -> JavaError {
+        let message_str = JavaLangString::from_rust_string(self, message).await.unwrap();
+        let instance = self.new_class(r#type, "(java/lang/String)V", (message_str,)).await.unwrap();
+
+        JavaError::JavaException(instance)
     }
 
     #[async_recursion::async_recursion(?Send)]
@@ -477,7 +564,8 @@ impl Jvm {
         }
     }
 
-    fn find_virtual_method(&self, class: &dyn ClassDefinition, name: &str, descriptor: &str, is_static: bool) -> Result<Box<dyn Method>> {
+    #[async_recursion::async_recursion(?Send)]
+    async fn find_virtual_method(&self, class: &dyn ClassDefinition, name: &str, descriptor: &str, is_static: bool) -> Result<Box<dyn Method>> {
         let method = class.method(name, descriptor);
 
         if let Some(x) = method {
@@ -486,8 +574,11 @@ impl Jvm {
             }
         } else if let Some(x) = class.super_class_name() {
             let super_class = self.classes.borrow().get(&x).unwrap().definition.clone();
-            return self.find_virtual_method(&*super_class, name, descriptor, is_static);
+            return self.find_virtual_method(&*super_class, name, descriptor, is_static).await;
         }
-        Err(JavaError::FatalError("NoSuchMethodError".into())) // TODO use real exception class
+
+        Err(self
+            .exception("java/lang/NoSuchMethodError", &format!("{}.{}:{}", class.name(), name, descriptor))
+            .await)
     }
 }
