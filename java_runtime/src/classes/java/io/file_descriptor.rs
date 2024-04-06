c55ea240707168e44624d18a8931ffb1a4a4729c
Inseok Lee <git@dlun.ch>
2024-04-06 10:37:18 +0900

    Create standard handles on file descriptor

--- java_runtime/src/classes/java/io/file_descriptor.rs
+++ java_runtime/src/classes/java/io/file_descriptor.rs
@@ -1,6 +1,7 @@
 use alloc::vec;
 
-use java_class_proto::JavaMethodProto;
+use java_class_proto::{JavaFieldProto, JavaMethodProto};
+use java_constants::FieldAccessFlags;
 use jvm::{ClassInstanceRef, Jvm, Result};
 
 use crate::{RuntimeClassProto, RuntimeContext};
@@ -13,13 +14,43 @@ impl FileDescriptor {
         RuntimeClassProto {
             parent_class: Some("java/lang/Object"),
             interfaces: vec![],
-            methods: vec![JavaMethodProto::new("<init>", "()V", Self::init, Default::default())],
-            fields: vec![],
+            methods: vec![
+                JavaMethodProto::new("<init>", "()V", Self::init, Default::default()),
+                JavaMethodProto::new("<clinit>", "()V", Self::cl_init, Default::default()),
+            ],
+            fields: vec![
+                JavaFieldProto::new("raw", "[B", Default::default()),
+                JavaFieldProto::new("err", "Ljava/io/FileDescriptor;", FieldAccessFlags::STATIC),
+                JavaFieldProto::new("in", "Ljava/io/FileDescriptor;", FieldAccessFlags::STATIC),
+                JavaFieldProto::new("out", "Ljava/io/FileDescriptor;", FieldAccessFlags::STATIC),
+            ],
         }
     }
 
+    async fn cl_init(jvm: &Jvm, runtime: &mut RuntimeContext) -> Result<()> {
+        tracing::debug!("java.io.FileDescriptor::<clinit>()");
+
+        let mut stderr = jvm.new_class("java/io/FileDescriptor", "()V", []).await?;
+        jvm.put_rust_object_field(&mut stderr, "raw", runtime.stderr().unwrap()).await?;
+
+        let mut stdin = jvm.new_class("java/io/FileDescriptor", "()V", []).await?;
+        jvm.put_rust_object_field(&mut stdin, "raw", runtime.stdin().unwrap()).await?;
+
+        let mut stdout = jvm.new_class("java/io/FileDescriptor", "()V", []).await?;
+        jvm.put_rust_object_field(&mut stdout, "raw", runtime.stdout().unwrap()).await?;
+
+        jvm.put_static_field("java/io/FileDescriptor", "err", "Ljava/io/FileDescriptor;", stderr)
+            .await?;
+        jvm.put_static_field("java/io/FileDescriptor", "in", "Ljava/io/FileDescriptor;", stdin)
+            .await?;
+        jvm.put_static_field("java/io/FileDescriptor", "out", "Ljava/io/FileDescriptor;", stdout)
+            .await?;
+
+        Ok(())
+    }
+
     async fn init(_jvm: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>) -> Result<()> {
-        tracing::debug!("java.io.FileDescriptor::<init>({:?})", &this,);
+        tracing::debug!("java.io.FileDescriptor::<init>({:?})", &this);
 
         Ok(())
     }
--- java_runtime/src/runtime/io.rs
+++ java_runtime/src/runtime/io.rs
@@ -1,5 +1,6 @@
 use alloc::boxed::Box;
 
+#[derive(Debug)]
 pub enum IOError {}
 pub type FileSize = u64;
 
@@ -9,7 +10,7 @@ pub struct FileStat {
 }
 
 #[async_trait::async_trait]
-pub trait File {
+pub trait File: Send {
     async fn read(&self, offset: FileSize, buf: &mut [u8]) -> Result<usize, IOError>;
     async fn write(&self, offset: FileSize, buf: &[u8]) -> Result<usize, IOError>;
     async fn stat(&self) -> Result<FileStat, IOError>;
