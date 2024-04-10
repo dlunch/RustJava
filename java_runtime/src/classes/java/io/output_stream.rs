df3b4b8204a53ea2dc1fb9b517d2bbf7caf30d2d
Inseok Lee <git@dlun.ch>
2024-04-10 10:20:54 +0900

    add methods to outputstream

--- java_runtime/src/classes/java/io/output_stream.rs
+++ java_runtime/src/classes/java/io/output_stream.rs
@@ -1,7 +1,7 @@
 use alloc::vec;
 
 use java_class_proto::JavaMethodProto;
-use jvm::{ClassInstanceRef, Jvm, Result};
+use jvm::{Array, ClassInstanceRef, Jvm, Result};
 
 use crate::{RuntimeClassProto, RuntimeContext};
 
@@ -13,7 +13,12 @@ impl OutputStream {
         RuntimeClassProto {
             parent_class: Some("java/lang/Object"),
             interfaces: vec![],
-            methods: vec![JavaMethodProto::new("<init>", "()V", Self::init, Default::default())],
+            methods: vec![
+                JavaMethodProto::new("<init>", "()V", Self::init, Default::default()),
+                JavaMethodProto::new("write", "([B)V", Self::write_bytes, Default::default()),
+                JavaMethodProto::new("write", "([BII)V", Self::write_bytes_offset, Default::default()),
+                JavaMethodProto::new_abstract("write", "(I)V", Default::default()),
+            ],
             fields: vec![],
         }
     }
@@ -22,6 +27,34 @@ impl OutputStream {
         tracing::debug!("java.io.OutputStream::<init>({:?})", &this);
 
         jvm.invoke_special(&this, "java/lang/Object", "<init>", "()V", ()).await?;
+
+        Ok(())
+    }
+
+    async fn write_bytes(jvm: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>, buffer: ClassInstanceRef<Array<i8>>) -> Result<()> {
+        tracing::debug!("java.io.OutputStream::write({:?}, {:?})", &this, &buffer);
+
+        let length = jvm.array_length(&buffer).await?;
+
+        jvm.invoke_virtual(&this, "write", "([BII)V", (buffer, 0, length as i32)).await?;
+
+        Ok(())
+    }
+
+    async fn write_bytes_offset(
+        jvm: &Jvm,
+        _: &mut RuntimeContext,
+        this: ClassInstanceRef<Self>,
+        buffer: ClassInstanceRef<Array<i8>>,
+        offset: i32,
+        length: i32,
+    ) -> Result<()> {
+        tracing::debug!("java.io.OutputStream::write({:?}, {:?}, {:?}, {:?})", &this, &buffer, &offset, &length);
+
+        let bytes = jvm.load_byte_array(&buffer, offset as _, length as _).await?;
+        for byte in bytes {
+            jvm.invoke_virtual(&this, "write", "(I)V", (byte as i32,)).await?;
+        }
 
         Ok(())
     }
