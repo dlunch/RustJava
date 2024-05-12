1d6e878936505fc717c117932bc56440c7a751b1
Inseok Lee <git@dlun.ch>
2024-05-12 15:12:48 +0900

    Add url constructors

--- java_runtime/src/classes/java/net/url.rs
+++ java_runtime/src/classes/java/net/url.rs
@@ -21,10 +21,23 @@ impl URL {
             parent_class: Some("java/lang/Object"),
             interfaces: vec![],
             methods: vec![
+                JavaMethodProto::new("<init>", "(Ljava/lang/String;)V", Self::init_with_spec, Default::default()),
+                JavaMethodProto::new(
+                    "<init>",
+                    "(Ljava/net/URL;Ljava/lang/String;)V",
+                    Self::init_with_context_spec,
+                    Default::default(),
+                ),
+                JavaMethodProto::new(
+                    "<init>",
+                    "(Ljava/net/URL;Ljava/lang/String;Ljava/net/URLStreamHandler;)V",
+                    Self::init_with_context_spec_handler,
+                    Default::default(),
+                ),
                 JavaMethodProto::new(
                     "<init>",
                     "(Ljava/lang/String;Ljava/lang/String;ILjava/lang/String;Ljava/net/URLStreamHandler;)V",
-                    Self::init,
+                    Self::init_with_protocol_host_port_file_handler,
                     Default::default(),
                 ),
                 JavaMethodProto::new("openConnection", "()Ljava/net/URLConnection;", Self::open_connection, Default::default()),
@@ -40,8 +53,55 @@ impl URL {
         }
     }
 
+    async fn init_with_spec(jvm: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>, spec: ClassInstanceRef<String>) -> Result<()> {
+        tracing::debug!("java.net.URL::<init>({:?}, {:?})", &this, &spec);
+
+        jvm.invoke_special(&this, "java/net/URL", "<init>", "(Ljava/net/URL;Ljava/lang/String;)V", (None, spec))
+            .await?;
+
+        Ok(())
+    }
+
+    async fn init_with_context_spec(
+        jvm: &Jvm,
+        _: &mut RuntimeContext,
+        this: ClassInstanceRef<Self>,
+        context: ClassInstanceRef<URL>,
+        spec: ClassInstanceRef<String>,
+    ) -> Result<()> {
+        tracing::debug!("java.net.URL::<init>({:?}, {:?}, {:?})", &this, &context, &spec);
+
+        jvm.invoke_special(
+            &this,
+            "java/net/URL",
+            "<init>",
+            "(Ljava/net/URL;Ljava/lang/String;Ljava/net/URLStreamHandler;)V",
+            (context, spec, None),
+        )
+        .await?;
+
+        Ok(())
+    }
+
+    async fn init_with_context_spec_handler(
+        jvm: &Jvm,
+        _: &mut RuntimeContext,
+        this: ClassInstanceRef<Self>,
+        context: ClassInstanceRef<URL>,
+        spec: ClassInstanceRef<String>,
+        handler: ClassInstanceRef<URLStreamHandler>,
+    ) -> Result<()> {
+        tracing::debug!("java.net.URL::<init>({:?}, {:?}, {:?}, {:?})", &this, &context, &spec, &handler);
+
+        jvm.invoke_special(&this, "java/lang/Object", "<init>", "()V", ()).await?;
+
+        // TODO
+
+        Ok(())
+    }
+
     #[allow(clippy::too_many_arguments)]
-    async fn init(
+    async fn init_with_protocol_host_port_file_handler(
         jvm: &Jvm,
         _: &mut RuntimeContext,
         mut this: ClassInstanceRef<Self>,
@@ -91,5 +151,31 @@ impl URL {
         let stream = jvm.invoke_virtual(&connection, "getInputStream", "()Ljava/io/InputStream;", ()).await?;
 
         Ok(stream)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use jvm::{runtime::JavaLangString, Result};
+
+    use crate::test::test_jvm;
+
+    #[futures_test::test]
+    async fn test_file_url() -> Result<()> {
+        let jvm = test_jvm().await?;
+
+        let url_spec = JavaLangString::from_rust_string(&jvm, "file:test.txt").await?;
+        let _url = jvm.new_class("java/net/URL", "(Ljava/lang/String;)V", (url_spec,)).await?;
+
+        /*
+        let connection = jvm.invoke_virtual(&url, "openConnection", "()Ljava/net/URLConnection;", ()).await?;
+        let stream = jvm.invoke_virtual(&connection, "getInputStream", "()Ljava/io/InputStream;", ()).await?;
+
+        let data: i32 = jvm.invoke_virtual(&stream, "read", "()I", ()).await?;
+
+        assert_eq!(data, 123);
+        */
+
+        Ok(())
     }
 }
