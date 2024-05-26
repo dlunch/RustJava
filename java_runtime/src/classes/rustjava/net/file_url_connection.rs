d6639758984365216cdfb27c88d313f79a84efc7
Inseok Lee <git@dlun.ch>
2024-05-26 09:41:05 +0900

    Add test on file url connection

--- java_runtime/src/classes/rustjava/net/file_url_connection.rs
+++ java_runtime/src/classes/rustjava/net/file_url_connection.rs
@@ -27,3 +27,32 @@ impl FileURLConnection {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use bytemuck::cast_vec;
+
+    use jvm::{runtime::JavaLangString, Result};
+
+    use crate::test::test_jvm_filesystem;
+
+    // #[futures_test::test]
+    async fn test_file_url() -> Result<()> {
+        let filesystem = [("test.txt".into(), b"test file content".to_vec())].into_iter().collect();
+        let jvm = test_jvm_filesystem(filesystem).await?;
+
+        let url_spec = JavaLangString::from_rust_string(&jvm, "file:test.txt").await?;
+        let url = jvm.new_class("java/net/URL", "(Ljava/lang/String;)V", (url_spec,)).await?;
+
+        let stream = jvm.invoke_virtual(&url, "openStream", "()Ljava/io/InputStream;", ()).await?;
+
+        let buf = jvm.instantiate_array("B", 16).await?;
+        let len: i32 = jvm.invoke_virtual(&stream, "read", "([B)I", (buf.clone(),)).await?;
+
+        let data = jvm.load_byte_array(&buf, 0, len as _).await?;
+
+        assert_eq!(cast_vec::<i8, u8>(data), b"test file content");
+
+        Ok(())
+    }
+}
