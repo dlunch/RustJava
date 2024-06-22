173ed3919c75a7e8877c6599a0bf9e1ae31b3c20
Inseok Lee <git@dlun.ch>
2024-06-22 10:35:00 +0900

    Parse manifest

--- java_runtime/src/classes/java/util/jar/manifest.rs
+++ java_runtime/src/classes/java/util/jar/manifest.rs
@@ -1,10 +1,10 @@
-use alloc::vec;
+use alloc::{vec, vec::Vec};
 
 use java_class_proto::{JavaFieldProto, JavaMethodProto};
-use jvm::{ClassInstanceRef, Jvm, Result};
+use jvm::{runtime::JavaLangString, ClassInstanceRef, Jvm, Result};
 
 use crate::{
-    classes::java::{io::InputStream, util::jar::Attributes},
+    classes::java::{io::InputStream, lang::String, util::jar::Attributes},
     RuntimeClassProto, RuntimeContext,
 };
 
@@ -40,8 +40,40 @@ impl Manifest {
         Ok(())
     }
 
-    async fn read(_jvm: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>, is: ClassInstanceRef<InputStream>) -> Result<()> {
+    async fn read(jvm: &Jvm, _: &mut RuntimeContext, mut this: ClassInstanceRef<Self>, is: ClassInstanceRef<InputStream>) -> Result<()> {
         tracing::debug!("java.util.jar.Manifest::read({:?}, {:?})", &this, &is);
+
+        // TODO we currently support only main attribute
+
+        let main_attributes = jvm.new_class("java/util/jar/Attributes", "()V", ()).await?;
+
+        let reader = jvm.new_class("java/io/InputStreamReader", "(Ljava/io/InputStream;)V", (is,)).await?;
+        let buffered_reader = jvm.new_class("java/io/BufferedReader", "(Ljava/io/Reader;)V", (reader,)).await?;
+
+        loop {
+            let line: ClassInstanceRef<String> = jvm.invoke_virtual(&buffered_reader, "readLine", "()Ljava/lang/String;", ()).await?;
+            if line.is_null() {
+                break;
+            }
+
+            let line = JavaLangString::to_rust_string(jvm, &line).await?;
+
+            let parts = line.splitn(2, ':').collect::<Vec<_>>();
+
+            let key = JavaLangString::from_rust_string(jvm, parts[0].trim()).await?;
+            let value = JavaLangString::from_rust_string(jvm, parts[1].trim()).await?;
+
+            let _: ClassInstanceRef<String> = jvm
+                .invoke_virtual(
+                    &main_attributes,
+                    "putValue",
+                    "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;",
+                    (key, value),
+                )
+                .await?;
+        }
+
+        jvm.put_field(&mut this, "attrs", "Ljava/util/jar/Attributes;", main_attributes).await?;
 
         Ok(())
     }
@@ -50,5 +82,41 @@ impl Manifest {
         tracing::debug!("java.util.jar.Manifest::getMainAttributes({:?})", &this);
 
         jvm.get_field(&this, "attrs", "Ljava/util/jar/Attributes;").await
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use bytemuck::cast_vec;
+
+    use jvm::{runtime::JavaLangString, Result};
+
+    use crate::test::test_jvm;
+
+    #[futures_test::test]
+    async fn test_manifest_parsing() -> Result<()> {
+        let jvm = test_jvm().await?;
+
+        let data = b"Main-Class: test";
+        let mut bytes = jvm.instantiate_array("B", data.len() as _).await?;
+        jvm.store_byte_array(&mut bytes, 0, cast_vec(data.to_vec())).await?;
+
+        let byte_array_stream = jvm.new_class("java/io/ByteArrayInputStream", "([B)V", (bytes,)).await?;
+        let manifest = jvm
+            .new_class("java/util/jar/Manifest", "(Ljava/io/InputStream;)V", (byte_array_stream,))
+            .await?;
+
+        let main_attributes = jvm
+            .invoke_virtual(&manifest, "getMainAttributes", "()Ljava/util/jar/Attributes;", ())
+            .await?;
+
+        let key = JavaLangString::from_rust_string(&jvm, "Main-Class").await?;
+        let value = jvm
+            .invoke_virtual(&main_attributes, "getValue", "(Ljava/lang/String;)Ljava/lang/String;", (key,))
+            .await?;
+
+        assert_eq!(JavaLangString::to_rust_string(&jvm, &value).await?, "test");
+
+        Ok(())
     }
 }
