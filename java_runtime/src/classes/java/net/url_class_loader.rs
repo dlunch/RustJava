d254a3d5465c16dccfffd4044d99d69aabe16368
Inseok Lee <git@dlun.ch>
2024-08-03 11:12:59 +0900

    Trim starting '/'

--- java_runtime/src/classes/java/net/url_class_loader.rs
+++ java_runtime/src/classes/java/net/url_class_loader.rs
@@ -158,6 +158,8 @@ impl URLClassLoader {
                 }
             } else {
                 // treat as jar
+                let name_str = name_str.trim_start_matches('/');
+
                 let jar_url_str = format!("jar:file:{}!/{}", file, name_str); // TODO url might not be file
                 let jar_url = JavaLangString::from_rust_string(jvm, &jar_url_str).await?;
                 let jar_url = jvm.new_class("java/net/URL", "(Ljava/lang/String;)V", (jar_url,)).await?;
@@ -207,6 +209,41 @@ mod test {
             .await?;
 
         let stream = jvm.invoke_virtual(&resource, "openStream", "()Ljava/io/InputStream;", ()).await?;
+
+        let buf = jvm.instantiate_array("B", 17).await?;
+        let len: i32 = jvm.invoke_virtual(&stream, "read", "([B)I", (buf.clone(),)).await?;
+
+        let data = jvm.load_byte_array(&buf, 0, len as _).await?;
+
+        assert_eq!(cast_vec::<i8, u8>(data), b"test content\n");
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_jar_loading_with_slash() -> Result<()> {
+        let jar = include_bytes!("../../../../../test_data/test.jar");
+        let filesystem = [("test.jar".into(), jar.to_vec())].into_iter().collect();
+        let jvm = test_jvm_filesystem(filesystem).await?;
+
+        let url_str = JavaLangString::from_rust_string(&jvm, "file:test.jar").await?;
+        let url = jvm.new_class("java/net/URL", "(Ljava/lang/String;)V", (url_str,)).await?;
+        let mut urls = jvm.instantiate_array("Ljava/net/URL;", 1).await?;
+        jvm.store_array(&mut urls, 0, vec![url]).await?;
+
+        let class_loader = jvm
+            .new_class("java/net/URLClassLoader", "([Ljava/net/URL;Ljava/lang/ClassLoader;)V", (urls, None))
+            .await?;
+
+        let resource_name = JavaLangString::from_rust_string(&jvm, "/test.txt").await?;
+        let stream = jvm
+            .invoke_virtual(
+                &class_loader,
+                "getResourceAsStream",
+                "(Ljava/lang/String;)Ljava/io/InputStream;",
+                (resource_name,),
+            )
+            .await?;
 
         let buf = jvm.instantiate_array("B", 17).await?;
         let len: i32 = jvm.invoke_virtual(&stream, "read", "([B)I", (buf.clone(),)).await?;
