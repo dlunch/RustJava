315885f4f306d0e3c22791f2022c312e01633439
blluv <biluv@protonmail.com>
2024-09-30 02:31:38 +0900

    Add java.util.Vector::lastIndexOf

--- java_runtime/src/classes/java/util/vector.rs
+++ java_runtime/src/classes/java/util/vector.rs
@@ -1,11 +1,12 @@
 use core::mem;
 
-use alloc::{sync::Arc, vec, vec::Vec};
+use alloc::vec;
+use alloc::{boxed::Box, format, sync::Arc, vec::Vec};
 
 use async_lock::Mutex;
 
 use java_class_proto::{JavaFieldProto, JavaMethodProto};
-use jvm::{ClassInstanceRef, Jvm, Result};
+use jvm::{ClassInstance, ClassInstanceRef, Jvm, Result};
 
 use crate::{classes::java::lang::Object, RuntimeClassProto, RuntimeContext};
 
@@ -35,6 +36,8 @@ impl Vector {
                 JavaMethodProto::new("remove", "(I)Ljava/lang/Object;", Self::remove, Default::default()),
                 JavaMethodProto::new("removeAllElements", "()V", Self::remove_all_elements, Default::default()),
                 JavaMethodProto::new("removeElementAt", "(I)V", Self::remove_element_at, Default::default()),
+                JavaMethodProto::new("lastIndexOf", "(Ljava/lang/Object;)I", Self::last_index_of, Default::default()),
+                JavaMethodProto::new("lastIndexOf", "(Ljava/lang/Object;I)I", Self::last_index_of_index, Default::default()),
             ],
             fields: vec![JavaFieldProto::new("raw", "[B", Default::default())],
         }
@@ -177,6 +180,59 @@ impl Vector {
         Ok(())
     }
 
+    async fn last_index_of(jvm: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>, element: ClassInstanceRef<Object>) -> Result<i32> {
+        tracing::debug!("java.util.Vector::lastIndexOf({:?}, {:?})", &this, &element);
+
+        let rust_vector = Self::get_rust_vector(jvm, &this).await?;
+        let index = rust_vector.lock().await.len() - 1;
+
+        let index: i32 = jvm
+            .invoke_virtual(&this, "lastIndexOf", "(Ljava/lang/Object;I)I", (element, index as i32))
+            .await?;
+
+        Ok(index)
+    }
+
+    async fn last_index_of_index(
+        jvm: &Jvm,
+        _: &mut RuntimeContext,
+        this: ClassInstanceRef<Self>,
+        element: ClassInstanceRef<Object>,
+        index: i32,
+    ) -> Result<i32> {
+        tracing::debug!("java.util.Vector::lastIndexOf({:?}, {:?}, {:?})", &this, &element, index);
+
+        let rust_vector = Self::get_rust_vector(jvm, &this).await?;
+        let size = rust_vector.lock().await.len();
+
+        if index as usize >= size {
+            return Err(jvm
+                .exception("java/lang/IndexOutOfBoundsException", &format!("{} >= {}", index, size))
+                .await);
+        }
+
+        if element.is_null() {
+            for i in (0..=index as usize).rev() {
+                let e: ClassInstanceRef<Object> = rust_vector.lock().await.get(i).unwrap().clone();
+                if e.is_null() {
+                    return Ok(i as i32);
+                }
+            }
+        } else {
+            let element: Box<dyn ClassInstance> = element.into();
+
+            for i in (0..=index as usize).rev() {
+                let e: Box<dyn ClassInstance> = rust_vector.lock().await.get(i).unwrap().clone().into();
+
+                if e.equals(&*element)? {
+                    return Ok(i as i32);
+                }
+            }
+        }
+
+        return Ok(-1);
+    }
+
     async fn get_rust_vector(jvm: &Jvm, this: &ClassInstanceRef<Self>) -> Result<RustVector> {
         jvm.get_rust_object_field(this, "raw").await
     }
@@ -239,6 +295,49 @@ mod test {
 
         let element_at: ClassInstanceRef<Object> = jvm.invoke_virtual(&vector, "elementAt", "(I)Ljava/lang/Object;", (0,)).await?;
         assert!(element_at.is_null());
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_vector_last_index_of() -> Result<()> {
+        let jvm = test_jvm().await?;
+
+        let vector = jvm.new_class("java/util/Vector", "()V", ()).await?;
+
+        let element1 = JavaLangString::from_rust_string(&jvm, "testValue1").await?;
+        let element2 = JavaLangString::from_rust_string(&jvm, "testValue2").await?;
+        let element3 = JavaLangString::from_rust_string(&jvm, "testValue3").await?;
+
+        let _: bool = jvm.invoke_virtual(&vector, "add", "(Ljava/lang/Object;)Z", (element1.clone(),)).await?;
+        let _: bool = jvm.invoke_virtual(&vector, "add", "(Ljava/lang/Object;)Z", (element2.clone(),)).await?;
+        let _: bool = jvm.invoke_virtual(&vector, "add", "(Ljava/lang/Object;)Z", (element3.clone(),)).await?;
+        let _: bool = jvm.invoke_virtual(&vector, "add", "(Ljava/lang/Object;)Z", (element2.clone(),)).await?;
+
+        let index: i32 = jvm
+            .invoke_virtual(&vector, "lastIndexOf", "(Ljava/lang/Object;)I", (element2.clone(),))
+            .await?;
+        assert_eq!(index, 3);
+
+        let index: i32 = jvm
+            .invoke_virtual(&vector, "lastIndexOf", "(Ljava/lang/Object;)I", (element1.clone(),))
+            .await?;
+        assert_eq!(index, 0);
+
+        let non_existing_element = JavaLangString::from_rust_string(&jvm, "nonExisting").await?;
+        let index: i32 = jvm
+            .invoke_virtual(&vector, "lastIndexOf", "(Ljava/lang/Object;)I", (non_existing_element,))
+            .await?;
+        assert_eq!(index, -1);
+
+        let index: i32 = jvm
+            .invoke_virtual(&vector, "lastIndexOf", "(Ljava/lang/Object;I)I", (element2.clone(), 2))
+            .await?;
+        assert_eq!(index, 1);
+
+        let _: bool = jvm.invoke_virtual(&vector, "add", "(Ljava/lang/Object;)Z", (None,)).await?;
+        let index: i32 = jvm.invoke_virtual(&vector, "lastIndexOf", "(Ljava/lang/Object;)I", (None,)).await?;
+        assert_eq!(index, 4);
 
         Ok(())
     }
