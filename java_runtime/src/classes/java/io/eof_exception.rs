af3ff3a5964e0f5699b925d21f788b7d7eceb553
Inseok Lee <git@dlun.ch>
2024-11-16 10:31:22 +0900

    Add missing exception class constructors

--- java_runtime/src/classes/java/io/eof_exception.rs
+++ java_runtime/src/classes/java/io/eof_exception.rs
@@ -1,6 +1,9 @@
 use alloc::vec;
 
-use crate::RuntimeClassProto;
+use java_class_proto::JavaMethodProto;
+use jvm::{ClassInstanceRef, Jvm, Result};
+
+use crate::{classes::java::lang::String, RuntimeClassProto, RuntimeContext};
 
 // class java.io.EOFException
 pub struct EOFException;
@@ -11,8 +14,29 @@ impl EOFException {
             name: "java/io/EOFException",
             parent_class: Some("java/io/IOException"),
             interfaces: vec![],
-            methods: vec![],
+            methods: vec![
+                JavaMethodProto::new("<init>", "()V", Self::init, Default::default()),
+                JavaMethodProto::new("<init>", "(Ljava/lang/String;)V", Self::init_with_message, Default::default()),
+            ],
             fields: vec![],
         }
+    }
+
+    async fn init(jvm: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>) -> Result<()> {
+        tracing::debug!("java.io.EOFException::<init>({:?})", &this);
+
+        let _: () = jvm.invoke_special(&this, "java/io/IOException", "<init>", "()V", ()).await?;
+
+        Ok(())
+    }
+
+    async fn init_with_message(jvm: &Jvm, _: &mut RuntimeContext, this: ClassInstanceRef<Self>, message: ClassInstanceRef<String>) -> Result<()> {
+        tracing::debug!("java.io.EOFException::<init>({:?}, {:?})", &this, &message);
+
+        let _: () = jvm
+            .invoke_special(&this, "java/io/IOException", "<init>", "(Ljava/lang/String;)V", (message,))
+            .await?;
+
+        Ok(())
     }
 }
