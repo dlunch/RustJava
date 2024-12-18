3526b4d65bd0d92f8b79ac4d262f12bba79ba19a
Inseok Lee <git@dlun.ch>
2024-12-18 16:06:39 +0900

    Add is_instance test

---
+++ jvm/tests/test_is_instance.rs
@@ -0,0 +1,16 @@
+use jvm::{runtime::JavaLangString, Result as JvmResult};
+
+use test_utils::test_jvm;
+
+#[tokio::test]
+async fn test_is_instance() -> JvmResult<()> {
+    let jvm = test_jvm().await?;
+
+    let string = JavaLangString::from_rust_string(&jvm, "test").await?;
+
+    assert!(jvm.is_instance(&*string, "java/lang/String"));
+    assert!(jvm.is_instance(&*string, "java/lang/Object"));
+    assert!(!jvm.is_instance(&*string, "java/lang/Integer"));
+
+    Ok(())
+}
