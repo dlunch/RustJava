f45d6444ecd6c11387f6ba171bc68a77310c6a5f
Inseok Lee <git@dlun.ch>
2025-11-02 09:07:53 +0900

    fmt

--- java_runtime/src/classes/java/lang.rs
+++ java_runtime/src/classes/java/lang.rs
@@ -1,6 +1,7 @@
 mod array_index_out_of_bounds_exception;
 mod class;
 mod class_loader;
+mod clone_not_supported_exception;
 mod cloneable;
 mod error;
 mod exception;
@@ -16,7 +17,6 @@ mod no_class_def_found_error;
 mod no_such_field_error;
 mod no_such_method_error;
 mod null_pointer_exception;
-mod clone_not_supported_exception;
 mod object;
 mod runnable;
 mod runtime;
@@ -30,12 +30,13 @@ mod throwable;
 mod unsupported_operation_exception;
 
 pub use self::{
-    array_index_out_of_bounds_exception::ArrayIndexOutOfBoundsException, class::Class, class_loader::ClassLoader, cloneable::Cloneable, error::Error,
-    exception::Exception, illegal_argument_exception::IllegalArgumentException, incompatible_class_change_error::IncompatibleClassChangeError,
+    array_index_out_of_bounds_exception::ArrayIndexOutOfBoundsException, class::Class, class_loader::ClassLoader,
+    clone_not_supported_exception::CloneNotSupportedException, cloneable::Cloneable, error::Error, exception::Exception,
+    illegal_argument_exception::IllegalArgumentException, incompatible_class_change_error::IncompatibleClassChangeError,
     index_out_of_bounds_exception::IndexOutOfBoundsException, instantiation_error::InstantiationError, integer::Integer,
     interrupted_exception::InterruptedException, linkage_error::LinkageError, math::Math, no_class_def_found_error::NoClassDefFoundError,
     no_such_field_error::NoSuchFieldError, no_such_method_error::NoSuchMethodError, null_pointer_exception::NullPointerException, object::Object,
     runnable::Runnable, runtime::Runtime, runtime_exception::RuntimeException, security_exception::SecurityException, string::String,
     string_buffer::StringBuffer, system::System, thread::Thread, throwable::Throwable,
-    unsupported_operation_exception::UnsupportedOperationException, clone_not_supported_exception::CloneNotSupportedException,
+    unsupported_operation_exception::UnsupportedOperationException,
 };
